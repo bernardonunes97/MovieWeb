08beaabdfdf08c85e6f4ba1f4659f3ab130d0809
Bernardo Nunes <bernardonunes97@gmail.com>
2020-06-04 11:32:25 -0300

    Movie+User bean

--- MovieWeb/src/br/com/movie/model/bean/MovieBean.java
+++ MovieWeb/src/br/com/movie/model/bean/MovieBean.java
@@ -1,5 +1,47 @@
 package br.com.movie.model.bean;
 
 public class MovieBean {
+	
+	//Properties
+	private int id;
+	private String name;
+	private float rating;
+	private String genre;
+	private int year;
+	private int runtime;
+	
+	//Constructor
+	public MovieBean(int id, String name, float rating, String genre, int year, int runtime) {
+		this.id = id;
+		this.name = name;
+		this.rating = rating;
+		this.genre = genre;
+		this.year = year;
+		this.runtime = runtime;
+	}
 
+	//Getters
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}	
 }
--- MovieWeb/src/br/com/movie/model/bean/UserBean.java
+++ MovieWeb/src/br/com/movie/model/bean/UserBean.java
@@ -1,5 +1,48 @@
 package br.com.movie.model.bean;
 
 public class UserBean {
+	
+	//properties
+	private int id;
+	private String username;
+	private String name;
+	private String password;
+	
+	//Constructor
+	public UserBean(int id, String username, String name, String password){
+		this.id = id;
+		this.username = username;
+		this.name = name;
+		this.password = password;
+	}
+
+	//Getters and Setters
+	public int getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
