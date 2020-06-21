acb84f3dfe67f6be3f13504d5a8d21eb26a94343
Lucas <lucashpc91@gmail.com>
2020-06-21 15:54:49 -0300

    Search consertado.

--- MovieWebFinal/MovieWebFinal/WebContent/scripts/MovieScript.js
+++ MovieWebFinal/MovieWebFinal/WebContent/scripts/MovieScript.js
@@ -32,8 +32,8 @@ function changeStateLikeButton(isFavorite) {
 $(document).ready(function(){
 	$("#txtSearch").on("keyup", function(){
 		var value = $(this).val().toLowerCase();
-		$(".card-container .card .card-body .card-text").filter(function(){
-			$(this).parent().parent().parent().toggle($(this).text().toLowerCase().indexOf(value)>=0)
+		$(".card .card-body .card-text").filter(function(){
+			$(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value)>=0)
 		});
 	});
 });
